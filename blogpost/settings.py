635fe53ebdae5ea60cf50756d701be0670a5d0a5
*avinashprajapati <62180007+AvinashPrajapati@users.noreply.github.com>
2023-03-31 03:09:22 +0530

    sa

--- blogpost/settings.py
+++ blogpost/settings.py
@@ -45,8 +45,8 @@ INSTALLED_APPS = [
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = "587"
-EMAIL_HOST_USER = "aviavi61106@gmail.com"
-EMAIL_HOST_PASSWORD = "hprrxycvhftyhkze"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = True
 
 MIDDLEWARE = [
